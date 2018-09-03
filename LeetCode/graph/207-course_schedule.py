b3a621d8c11c13701e773cb504f32419cd558c17
David Lin <davll.xc@gmail.com>
2018-09-03 23:54:52 +0800

    [leet207] add dfs impl

--- LeetCode/graph/207-course_schedule.py
+++ LeetCode/graph/207-course_schedule.py
@@ -1,5 +1,3 @@
-# [WIP]
-
 def edges_to_adjacencies(n, edges):
     adj = [[] for _ in range(n)]
     for pair in edges:
@@ -7,7 +5,27 @@ def edges_to_adjacencies(n, edges):
     return adj
 
 def detect_cycle(n, adj):
-    
+    pendings = set(range(0, n))
+    visited, stacked = [False] * n, [False] * n
+    # define Depth-First Search
+    def dfs(v):
+        if not visited[v]:
+            pendings.discard(v)
+            visited[v] = True
+            stacked[v] = True
+            for t in adj[v]:
+                if not visited[t] and dfs(t):
+                    return True
+                elif stacked[t]:
+                    return True
+        stacked[v] = False
+        return False
+    # scan the graph
+    while len(pendings) > 0:
+        v = pendings.pop()
+        if dfs(v):
+            return True
+    return False
 
 class Solution:
     def canFinish(self, numCourses, prerequisites):
