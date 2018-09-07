0b827ab482812d5770c91fa601c3592533e46491
David Lin <davll.xc@gmail.com>
2018-09-07 19:37:44 +0800

    add floyd warshall

--- Algorithms/graph/floyd_warshall.py
+++ Algorithms/graph/floyd_warshall.py
@@ -1,10 +1,12 @@
+import math
+
 def floyd_warshall(n, edges):
     """
     n: int
     edges: [(u, v, w)]
     return: [[w]]
     """
-    dist = [[float('inf')] * n for _ in range(n)]
+    dist = [[math.inf] * n for _ in range(n)]
     for (u,v,w) in edges:
         dist[u][v] = w
     for v in range(n):
@@ -12,4 +14,5 @@ def floyd_warshall(n, edges):
     for k in range(0, n):
         for i in range(0, n):
             for j in range(0, n):
-                dist[i][j] = max(dist[i][j], dist[i][k] + dist[k][j])
+                dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
+    return dist
---
+++ HackerRank/floyd-city_of_blinding_lights.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import sys
+
+def floyd_warshall(n, edges):
+    dist = [[math.inf] * n for _ in range(n)]
+    for (u,v,w) in edges:
+        dist[u][v] = w
+    for v in range(n):
+        dist[v][v] = 0
+    for k in range(0, n):
+        for i in range(0, n):
+            for j in range(0, n):
+                dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
+    return dist
+
+if __name__ == '__main__':
+    road_nodes, road_edges = map(int, input().split())
+    edges = [tuple(map(int, input().split())) for _ in range(road_edges) ]
+    for i in range(road_edges):
+        (u,v,w) = edges[i]
+        edges[i] = (u-1,v-1,w)
+    dist = floyd_warshall(road_nodes, edges)
+
+    q = int(input())
+    for _ in range(q):
+        x, y = map(int, input().split())
+        d = dist[x-1][y-1]
+        if math.isinf(d):
+            print("-1")
+        else:
+            print(str(d))
