efc97ceaf7b3a4f6bd126740d50f408144a84065
David Lin <davll.xc@gmail.com>
2018-10-10 02:16:21 +0800

    add kadane algo info links

--- algo.py/algo/dp/maxsum.py
+++ algo.py/algo/dp/maxsum.py
@@ -7,6 +7,14 @@
 # f[i] = a[0] if i = 0
 #      = max { a[i], f[i-1] + a[i] } if i > 0
 #
+# Refs:
+# https://www.geeksforgeeks.org/largest-sum-contiguous-subarray/
+# https://en.wikipedia.org/wiki/Maximum_subarray_problem#Kadane's_algorithm
+# https://www.geeksforgeeks.org/maximum-sum-rectangle-in-a-2d-matrix-dp-27/
+#
+# Problems:
+# https://practice.geeksforgeeks.org/problems/kadanes-algorithm/0
+#
 def max_subarray(a):
     result, c = a[0], a[0]
     for x in a[1:]:
