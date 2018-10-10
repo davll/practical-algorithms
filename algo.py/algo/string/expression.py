b66cfe2600a09c671a705ac8bebc3dddbb621eec
David Lin <davll.xc@gmail.com>
2018-10-10 12:08:19 +0800

    yooooo

---
+++ algo.py/algo/string/expression.py
@@ -0,0 +1,11 @@
+# Expression Parsing
+
+# Shunting-yard Algorithm
+#
+# http://www.oxfordmathcenter.com/drupal7/node/628
+# https://en.wikipedia.org/wiki/Shunting-yard_algorithm
+def infix_to_postfix(expr):
+    pass
+
+def eval_postfix(expr):
+    pass
--- algo.py/algo/wip/trap_water.py
+++ algo.py/algo/wip/trap_water.py
@@ -1,7 +1,3 @@
 # Trap Water
 # https://www.geeksforgeeks.org/trapping-rain-water/
 # https://leetcode.com/problems/trapping-rain-water/description/
-
-#
-# 
-#
