21c9cbe8aee55bfed1c2e4ca2d9f8bf718827ab2
David Lin <davll.xc@gmail.com>
2018-10-19 08:56:19 +0800

    wip: working on expression parsing

--- algo.py/algo/string/expression.py
+++ algo.py/algo/string/expression.py
@@ -1,11 +1,22 @@
 # Expression Parsing
 
+from collections import namedtuple
+
+Op = namedtuple('Op', ['name', 'symbol', 'precedence'])
+
+OPERATORS = {
+    '+': Op('add', '+', 0),
+    '-': Op('sub', '-', 0),
+    '*': Op('mul', '*', 0),
+    '/': Op('div', '/', 0),
+}
+
 # Shunting-yard Algorithm
 #
 # http://www.oxfordmathcenter.com/drupal7/node/628
 # https://en.wikipedia.org/wiki/Shunting-yard_algorithm
-def infix_to_postfix(expr):
+def infix_to_postfix(tokens):
     pass
 
-def eval_postfix(expr):
+def eval_postfix(tokens):
     pass
