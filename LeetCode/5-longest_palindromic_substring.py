56a940308982b688693d1d8cf4214413abcdde27
David Lin <davll.xc@gmail.com>
2018-10-30 02:12:53 +0800

    wip: longest palindrome

--- LeetCode/5-longest_palindromic_substring.py
+++ LeetCode/5-longest_palindromic_substring.py
@@ -5,6 +5,10 @@
 #
 # P[i,j] = (if the sub-string s[i:j+1] is a palindrome)
 #
+# P[i,j] = P[i+1,j-1]               if s[i] = s[j]
+#        = True                     if i = j
+#        = bool(s[i] == s[j])       if j = i+1
+#
 def longest_palindrome_v1(text):
     n = len(text)
     dp = [[False] * n for _ in range(n)]
@@ -12,9 +16,36 @@ def longest_palindrome_v1(text):
         dp[i][i] = True
     for i in range(n-1):
         dp[i][i+1] = (text[i] == text[i+1])
-    for k in range(3,n):
-        for i in range(n):
-            ###
+    for k in range(3,n+1):
+        for i in range(n-k+1):
+            j = i + k - 1
+            dp[i][j] = (text[i] == text[j] and dp[i+1][j-1])
+    maxlen = 0
+    for i in range(n):
+        for j in range(i,n):
+            if dp[i][j]:
+                maxlen = max(maxlen, j-i+1)
+    for i in range(n-maxlen+1):
+        j = i + maxlen - 1
+        if dp[i][j]:
+            return text[i:j+1]
+    return None
+
+# Idea: Expand around center
+def longest_palindrome_v2(text):
+    pass
+
+# Idea: Manacher's Algorithm
+def longest_palindrome_v3(text):
+    from itertools import chain
+    s = '#'.join(chain('^', s, '$')
+    n = len(s)
+    p = [0] * n
+    c, r = 0, 0
+    for i in range(1, n-1):
+        j = 2 * c - i
+        p[i] = min(r-i, p[j]) if r > i else 0
+        #while s
 
 class Solution:
     def longestPalindrome(self, s):
@@ -22,5 +53,8 @@ class Solution:
         :type s: str
         :rtype: str
         """
+        if not s:
+            return ""
+        return longest_palindrome_v1(s)
         
 # http://www.cnblogs.com/grandyang/p/4475985.html
--- algo.py/algo/search/histogram.py
+++ algo.py/algo/search/histogram.py
@@ -1,20 +1,20 @@
 # Window sliding
 
-def previous_smaller(arr):
-    # prev[i] = index of previous element that is smaller than arr[i]
-    # => likewise, all elements in arr[prev[i]+1:i] are greater or equal to arr[i]
+def left_span(arr):
+    # left[i] = index of previous element that is smaller than arr[i]
+    # => likewise, all elements in arr[left[i]+1:i] are greater or equal to arr[i]
     n = len(arr)
     stack = []
-    prev = [-1] * n
+    left = [-1] * n
     for i in range(n):
         while stack and arr[stack[-1]] >= arr[i]:
             stack.pop()
         if stack:
-            prev[i] = stack[-1]
+            left[i] = stack[-1]
         stack.append(i)
-    return prev
+    return left
 
-def next_smaller(arr):
+def right_span(arr):
     # right[i] = index of next element that is smaller than arr[i]
     # => likewise, all elements in arr[i+1:right[i]] are greater or equal to arr[i]
     n = len(arr)
