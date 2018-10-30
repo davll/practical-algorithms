242e8b7047795ccb388d5a0f81deb70eaaa402a4
David Lin <davll.xc@gmail.com>
2018-10-30 13:12:05 +0800

    solved problems

--- LeetCode/76-minimum_window_substring.py
+++ LeetCode/76-minimum_window_substring.py
@@ -1,4 +1,61 @@
+# https://leetcode.com/problems/minimum-window-substring/
+# https://leetcode.com/problems/minimum-window-substring/discuss/26804/12-lines-Python
+# https://leetcode.com/problems/minimum-window-substring/discuss/26808/Here-is-a-10-line-template-that-can-solve-most-'substring'-problems
+# https://leetcode.com/problems/minimum-window-substring/discuss/172339/Accepted-Java-Sliding-Window-O(N)-using-1-HashMap
+
 from collections import deque, Counter
+
+def min_window_v1(text, letters):
+    required = Counter(letters)
+    count = Counter()
+    positions = {}
+    queue = deque()
+    ans = None
+    for i, c in enumerate(text):
+        if c in required:
+            positions[c] = i
+            count[c] += 1
+        while queue:
+            x = queue[0]
+            if x < positions[text[x]] and count[text[x]] > required[text[x]]:
+                queue.popleft()
+                count[text[x]] -= 1
+            else:
+                break
+        if c in required:
+            queue.append(i)
+        if all(map(lambda k: count[k] >= required[k], required.keys())):
+            if not ans or (queue[-1]-queue[0]) < (ans[1]-ans[0]):
+                ans = (queue[0], queue[-1])
+    if ans:
+        return text[ans[0]:ans[1]+1]
+    else:
+        return ""
+
+def min_window_v2(text, letters):
+    required = Counter(letters)
+    missings = len(letters)
+    i = 0
+    ans = None
+    for j, c in enumerate(text, 1):
+        # consider a window = text[i:j]
+        if required[c] > 0:
+            missings -= 1
+        required[c] -= 1
+        # if all required chars are found
+        if missings == 0:
+            # drain unnecessary chars to shrink the window
+            while i < j and required[text[i]] < 0:
+                required[text[i]] += 1
+                i += 1
+            # update answer
+            if not ans or j-i <= (ans[1]-ans[0]):
+                ans = (i, j)
+    if ans:
+        i, j = ans
+        return text[i:j]
+    else:
+        return ""
 
 class Solution:
     def minWindow(self, s, t):
@@ -7,29 +64,4 @@ class Solution:
         :type t: str
         :rtype: str
         """
-        text = s
-        required = Counter(t)
-        count = Counter()
-        positions = {}
-        queue = deque()
-        ans = None
-        for i, c in enumerate(text):
-            if c in required:
-                positions[c] = i
-                count[c] += 1
-            while queue:
-                x = queue[0]
-                if x < positions[text[x]] and count[text[x]] > required[text[x]]:
-                    queue.popleft()
-                    count[text[x]] -= 1
-                else:
-                    break
-            if c in required:
-                queue.append(i)
-            if all(map(lambda k: count[k] >= required[k], required.keys())):
-                if not ans or (queue[-1]-queue[0]) < (ans[1]-ans[0]):
-                    ans = (queue[0], queue[-1])
-        if ans:
-            return text[ans[0]:ans[1]+1]
-        else:
-            return ""
+        return min_window_v2(s, t)
--- LeetCode/901-online_stock_span.py
+++ LeetCode/901-online_stock_span.py
@@ -4,12 +4,23 @@
 class StockSpanner:
     def __init__(self):
         self.stack = []
+        self.count = 0
     def next(self, price):
         """
         :type price: int
         :rtype: int
         """
-        
+        curr = last = self.count
+        self.count += 1
+        while self.stack:
+            i, p = self.stack[-1]
+            if p <= price:
+                self.stack.pop()
+                last = i
+            else:
+                break
+        self.stack.append((last, price))
+        return curr - last + 1
 
 
 # Your StockSpanner object will be instantiated and called as such:
