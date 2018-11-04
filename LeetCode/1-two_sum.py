7f12b94ddc440e2eb97ba8cf7fac83e3e4da000a
David Lin <davll.xc@gmail.com>
2018-11-05 02:15:31 +0800

    fix minor

--- LeetCode/1-two_sum.py
+++ LeetCode/1-two_sum.py
@@ -7,7 +7,18 @@ def two_sum_v1(nums, target):
             lut[target - nums[i]] = i
 
 def two_sum_v2(nums, target):
-    pass
+    n = len(nums)
+    index = list(range(n))
+    index.sort(key = lambda i: nums[i])
+    l, r = 0, n-1
+    while l < r:
+        i, j = index[l], index[r]
+        if nums[i] + nums[j] == target:
+            return [i, j]
+        elif nums[i] + nums[j] < target:
+            l += 1
+        else: # nums[i] + nums[j] > target
+            r -= 1
 
 class Solution:
     def twoSum(self, nums, target):
@@ -16,4 +27,4 @@ class Solution:
         :type target: int
         :rtype: List[int]
         """
-        
+        return two_sum_v2(nums, target)
