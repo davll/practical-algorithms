609482bdff5bc03909738824624cacbe946115f3
David Lin <davll.xc@gmail.com>
2018-11-06 20:37:24 +0800

    wtf

--- LeetCode/454-4sum_ii.py
+++ LeetCode/454-4sum_ii.py
@@ -1,11 +1,9 @@
 from collections import Counter
 
+# Idea: Hashing
 def four_sum_count_v1(A, B, C, D):
     ab = Counter(a+b for a in A for b in B)
     return sum(ab[-c-d] for c in C for d in D)
-
-def four_sum_count_v2(A, B, C, D):
-    pass
 
 class Solution(object):
     def fourSumCount(self, A, B, C, D):
