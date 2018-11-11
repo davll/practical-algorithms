f79e166b4e35023840a01e7475b2d69263095953
David Lin <davll.xc@gmail.com>
2018-11-12 03:13:35 +0800

    more solutions

--- LeetCode/135-candy.py
+++ LeetCode/135-candy.py
@@ -15,10 +15,60 @@ def dist_candy_v1(scores):
         candy[i] = c + 1
     return sum(candy)
 
+# Idea: Dynamic Programming
+#
+# suppose a[0:n] is non-decreasing
+#
+# m[i] => the cost at a[i]
+#
+# m[i] = m[i-1] + 1                  if a[i-1] < a[i]
+#      = 1                           if a[i-1] = a[i]
+#      = 1                           if i = 0
+#      = 1                           otherwise
+#
+# then generalize m[i] to allow increasing and decreasing
+#
+# m1[i] = m1[i-1] + 1       (forward direction)
+# m2[i] = m2[i+1] - 1       (reverse direction)
+# m[i] = max(m1[i], m2[i])
+#
+# then answer = sum(m[:])
+#
+# case 1: [10 10 10 10 10] => [1 1 1 1 1]
+# case 2: [10 9 8 7 6] => [5 4 3 2 1]
+# case 3: [1 2 2] => [1 2 1]
+# case 4: [5 6 7 8 9] => [1 2 3 4 5]
+# case 5: [5 6 6 8 9] => [1 2 1 2 3]
+# case 6: [1 2 4 3 2 1] => [1 2 4 3 2 1]
+# case 7: [1 2 4 4 3 2 1] => [1 2 3 4 3 2 1]
+# case 8: [2 4 2 6 1 7 8 9 2 1] => [1 2 1 2 1 2 3 4 2 1]
+# case 9: [2 4 3 5 2 6 4 5] => [1 2 1 2 1 2 1 2]
+#
+def dist_candy_v2(scores):
+    n = len(scores)
+    m1, m2 = [0] * n, [0] * n
+    m1[0] = 1
+    m2[-1] = 1
+    for i in range(1,n):
+        if scores[i-1] < scores[i]:
+            m1[i] = m1[i-1] + 1
+        elif scores[i-1] == scores[i]:
+            m1[i] = 1
+        else:
+            m1[i] = 1
+    for i in range(n-2,-1,-1):
+        if scores[i+1] < scores[i]:
+            m2[i] = m2[i+1] + 1
+        elif scores[i+1] == scores[i]:
+            m2[i] = 1
+        else:
+            m2[i] = 1
+    return sum(map(max, zip(m1, m2)))
+
 class Solution:
     def candy(self, ratings):
         """
         :type ratings: List[int]
         :rtype: int
         """
-        return dist_candy_v1(ratings)
+        return dist_candy_v2(ratings)
--- LeetCode/42-trapping_rain_water.py
+++ LeetCode/42-trapping_rain_water.py
@@ -2,41 +2,41 @@
 
 # Idea: Dynamic Programming
 # T = O(n)
-def trap_water_v2(height):
-    n = len(height)
+def trap_water_v2(heights):
+    n = len(heights)
     left = [0] * n
     right = [n-1] * n
     # compute left barriers
     curr_left = 0
     for i in range(1, n):
-        if height[i] >= height[curr_left]:
+        if heights[i] >= heights[curr_left]:
             curr_left = i
         left[i] = curr_left
     # compute right barriers
     curr_right = n-1
     for i in range(n-2,-1,-1):
-        if height[i] >= height[curr_right]:
+        if heights[i] >= heights[curr_right]:
             curr_right = i
         right[i] = curr_right
     #
     result = 0
     for i in range(n):
-        h = min(height[left[i]], height[right[i]])
-        result += max(0, h - height[i])
+        h = min(heights[left[i]], heights[right[i]])
+        result += max(0, h - heights[i])
     return result
 
 # Idea: BFS
-def trap_water_v3(height):
+def trap_water_v3(heights):
     from heapq import heappush, heappop, heapify
     #
-    n = len(height)
+    n = len(heights)
     heap = []
     visited = [False] * n
     # visit borders
     for i in (0, n-1):
         if i not in range(n):
             continue
-        heap.append((height[i], i))
+        heap.append((heights[i], i))
         visited[i] = True
     heapify(heap)
     # visit cells
@@ -52,9 +52,42 @@ def trap_water_v3(height):
             if ni not in range(n):
                 continue
             if not visited[ni]:
-                heappush(heap, (height[ni], ni))
+                heappush(heap, (heights[ni], ni))
                 visited[ni] = True
     return water
+
+# Idea: Stack
+def trap_water_v4(heights):
+    n = len(heights)
+    result = 0
+    stack = []
+    for i in range(n):
+        while stack and heights[stack[-1]] <= heights[i]:
+            t = stack.pop()
+            if not stack:
+                break
+            w = i - stack[-1] - 1
+            h = min(heights[i], heights[stack[-1]]) - heights[t]
+            result += w * h
+        stack.append(i)
+    return result
+
+# Idea: 2 Pointers
+def trap_water_v5(heights):
+    n = len(heights)
+    l, r = 0, n-1
+    lmax, rmax = 0, 0
+    result = 0
+    while l < r:
+        if heights[l] < heights[r]:
+            result += max(0, lmax - heights[l])
+            lmax = max(lmax, heights[l])
+            l += 1
+        else:
+            result += max(0, rmax - heights[r])
+            rmax = max(rmax, heights[r])
+            r -= 1
+    return result
 
 class Solution:
     def trap(self, height):
@@ -62,4 +95,4 @@ class Solution:
         :type height: List[int]
         :rtype: int
         """
-        return trap_water_v3(height)
+        return trap_water_v5(height)
