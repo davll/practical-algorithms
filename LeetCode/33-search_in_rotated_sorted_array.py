4cdf0f50312970169f9d42f61cb0f63fabaf3947
David Lin <davll.xc@gmail.com>
2018-11-13 22:37:55 +0800

    solved 33

--- LeetCode/33-search_in_rotated_sorted_array.py
+++ LeetCode/33-search_in_rotated_sorted_array.py
@@ -5,22 +5,17 @@ def search_v1(nums, target):
 
 def search_v2(nums, target):
     n = len(nums)
+    s = find_min(nums)
+    assert s >= 0
     l, r = 0, n-1
     while l <= r:
         m = (l + r) // 2
-        if nums[m] == target:
-            return m
-        elif nums[m] < target:
-            if target <= nums[r]: # M < target <= R => in M..R
-                l = m+1
-            elif nums[l] <= nums[m]: # R < L <= M < target => in M..R
-                r = m-1
-            elif nums[m]: # M < R < L => 
-        else: # nums[m] > target
-            if target >= nums[l]: # L <= target < M
-                r = m-1
-            # R < L < M => 2 3 1
-            # M < R < L => 3 1 2
+        if nums[(m+s)%n] == target:
+            return (m+s)%n
+        elif nums[(m+s)%n] < target:
+            l = m+1
+        else:
+            r = m-1
     return -1
 
 def find_min(nums):
