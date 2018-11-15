9b5f09bfc16b3f76d44c100b4362392e50d464e0
David Lin <davll.xc@gmail.com>
2018-11-15 20:11:22 +0800

    solved 59

--- LeetCode/59-spiral_matrix_ii.py
+++ LeetCode/59-spiral_matrix_ii.py
@@ -1,6 +1,33 @@
+# https://leetcode.com/problems/spiral-matrix-ii/
+
 def spiral_fill(n):
     mat = [[0] * n for _ in range(n)]
-    row0, row1, col0, col1 = 0, n-1, 0, n-1
+    i, j = 0, 0
+    mat[i][j] = 1
+    repeat = True
+    while repeat:
+        repeat = False
+        # +j direction
+        while j+1 < n and mat[i][j+1] == 0:
+            mat[i][j+1] = mat[i][j] + 1
+            j = j + 1
+            repeat = True
+        # +i direction
+        while i+1 < n and mat[i+1][j] == 0:
+            mat[i+1][j] = mat[i][j] + 1
+            i = i + 1
+            repeat = True
+        # -j direction
+        while j-1 >= 0 and mat[i][j-1] == 0:
+            mat[i][j-1] = mat[i][j] + 1
+            j = j - 1
+            repeat = True
+        # -i direction
+        while i-1 >= 0 and mat[i-1][j] == 0:
+            mat[i-1][j] = mat[i][j] + 1
+            i = i - 1
+            repeat = True
+    return mat
 
 class Solution:
     def generateMatrix(self, n):
