d213665063d472a28318045df24e64d429318e28
David Lin <davll.xc@gmail.com>
2018-11-23 00:18:39 +0800

    solved stocks

--- LeetCode/123-best_time_to_buy_and_sell_stock_iii.py
+++ LeetCode/123-best_time_to_buy_and_sell_stock_iii.py
@@ -5,22 +5,22 @@
 # Idea: Dynamic Programming
 #
 # s0: start
-# s1: first bought
-# s2: first sold
-# s3: second bought
-# s4: second sold
+# b1: first bought
+# s1: first sold
+# b2: second bought
+# s2: second sold
 #
 # s0[i] = s0[i-1]
-# s1[i] = max(s1[i-1], s0[i-1] - prices[i])
-# s2[i] = max(s2[i-1], s1[i-1] + prices[i])
-# s3[i] = max(s3[i-1], s2[i-1] - prices[i])
-# s4[i] = max(s4[i-1], s3[i-1] + prices[i])
+# b1[i] = max(b1[i-1], s0[i-1] - prices[i])
+# s1[i] = max(s1[i-1], b1[i-1] + prices[i])
+# b2[i] = max(b2[i-1], s1[i-1] - prices[i])
+# s2[i] = max(s2[i-1], b2[i-1] + prices[i])
 #
 # s0[0] = 0
-# s1[0] = -prices[0]
+# b1[0] = -prices[0]
+# s1[0] = 0
+# b2[0] = -prices[0]
 # s2[0] = 0
-# s3[0] = -prices[0]
-# s4[0] = 0
 #
 def max_profit_v1(prices):
     n = len(prices)
@@ -35,6 +35,76 @@ def max_profit_v1(prices):
         s4[i] = max(s4[i-1], s3[i-1] + prices[i])
     return s4[-1]
 
+# Idea: Dynamic Programming
+#
+# F[k;i] = max {
+#              # do nothing
+#              F[k;i-1]
+#
+#              # buy on day 0, sell on day i
+#              prices[i] - prices[0]
+#
+#              # buy on day j, sell on day i
+#              prices[i] - prices[j] + F[k-1;j-1] for 1 <= j < i
+#
+#              # btw, j can be i as well => miss the k-th transaction
+#          }
+#
+def max_profit_v2(prices):
+    N = len(prices)
+    K = 2
+    F = [[0] * N for _ in range(K+1)]
+    # profit = 0 when k = 0
+    for k in range(1, K+1):
+        for i in range(1, N):
+            # buy on day 0, sell on day i
+            minval = prices[0]
+            for j in range(1, i):
+                # buy on day j, sell on day i
+                minval = min(minval, prices[j] - F[k-1][j-1])
+            # sell on day i or not
+            F[k][i] = max(F[k][i-1], prices[i] - minval)
+    return F[K][-1]
+
+# Improved over v2
+#
+# repeated computation on minval can be eliminated
+#
+def max_profit_v3(prices):
+    N = len(prices)
+    K = 2
+    F = [[0] * N for _ in range(K+1)]
+    for k in range(1, K+1):
+        minval = prices[0]
+        for i in range(1, N):
+            F[k][i] = max(F[k][i-1], prices[i] - minval)
+            minval = min(minval, prices[i] - F[k-1][i-1])
+    return F[K][-1]
+
+# Swap the two for-loops
+def max_profit_v4(prices):
+    N = len(prices)
+    K = 2
+    F = [[0] * N for _ in range(K+1)]
+    M = [prices[0]] * (K+1)
+    for i in range(1, N):
+        for k in range(1, K+1):
+            F[k][i] = max(F[k][i-1], prices[i] - M[k])
+            M[k] = min(M[k], prices[i] - F[k-1][i-1])
+    return F[K][-1]
+
+# optimize space
+def max_profit_v5(prices):
+    N = len(prices)
+    K = 2
+    F = [0] * (K+1)
+    M = [prices[0]] * (K+1)
+    for i in range(1, N):
+        for k in range(1, K+1):
+            F[k] = max(F[k], prices[i] - M[k])
+            M[k] = min(M[k], prices[i] - F[k-1])
+    return F[K]
+
 class Solution:
     def maxProfit(self, prices):
         """
@@ -43,4 +113,4 @@ class Solution:
         """
         if not prices:
             return 0
-        return max_profit_v1(prices)
+        return max_profit_v5(prices)
--- LeetCode/188-best_time_to_buy_and_sell_stock_iv.py
+++ LeetCode/188-best_time_to_buy_and_sell_stock_iv.py
@@ -1,3 +1,37 @@
 # https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iv/description/
 
+# Idea: Dynamic Programming
+#
+# B[k;i] = max(B[k;i-1], S[k-1;i-1] - prices[i])
+# S[k;i] = max(S[k;i-1], B[k;i-1] + prices[i])
+#
+# S[0;i] = 0
+# S[k;0] = 0
+# B[k;0] = -prices[0]
+#
+def max_profit_v1(K, prices):
+    N = len(prices)
+    S0, S1 = ([0] * (K+1) for _ in range(2))
+    B0, B1 = ([0] * (K+1) for _ in range(2))
+    for k in range(1, K+1):
+        B0[k] = -prices[0]
+    for i in range(1, N):
+        for k in range(1, K+1):
+            B1[k] = max(B0[k], S0[k-1] - prices[i])
+            S1[k] = max(S0[k], B0[k] + prices[i])
+        S0, S1 = S1, S0
+        B0, B1 = B1, B0
+    return S0[K]
 
+class Solution:
+    def maxProfit(self, k, prices):
+        """
+        :type k: int
+        :type prices: List[int]
+        :rtype: int
+        """
+        if not prices:
+            return 0
+        if k >= len(prices):
+            return sum(max(0, a-b) for a, b in zip(prices[1:], prices[:-1]))
+        return max_profit_v1(k, prices)
